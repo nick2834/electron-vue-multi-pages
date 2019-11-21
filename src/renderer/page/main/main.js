e7d0b8162374c1670a4f4d4f08effddd2473fdda
Nick Xu <283448189@qq.com>
2019-11-21 22:56:09 +0800

    增加拖拽组件

--- src/renderer/page/main/main.js
+++ src/renderer/page/main/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from '@/store'
+import './directives.js'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/style/element-variables.scss';
